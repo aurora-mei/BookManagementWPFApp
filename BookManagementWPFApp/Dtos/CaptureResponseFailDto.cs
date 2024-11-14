bc1b6f6a7160aed744895305490c3a1b34712573
Aimkey <phamminhkiet24@gmail.com>
2024-11-15 02:56:37 +0700

    feat: added paypal payment for da page

---
+++ BookManagementWPFApp/Dtos/CaptureResponseFailDto.cs
@@ -0,0 +1,23 @@
+﻿namespace BookManagementWPFApp.Dtos;
+
+public class CaptureResponseFailDto
+{
+    public string name { get; set; }
+    public Details[] details { get; set; }
+    public string message { get; set; }
+    public string debug_id { get; set; }
+    public Links[] links { get; set; }
+
+    public class Details
+    {
+        public string issue { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Links
+    {
+        public string href { get; set; }
+        public string rel { get; set; }
+        public string method { get; set; }
+    }
+}
--- BookManagementWPFApp/PaymentWindow.xaml.cs
+++ BookManagementWPFApp/PaymentWindow.xaml.cs
@@ -1,88 +1,125 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using BookManagementWPFApp.Dtos;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Windows;
+using BookManagement.BusinessObjects;
+using BookManagement.DataAccess.Repositories;
+using BookManagementWPFApp.Constants;
+using BookManagementWPFApp.Dtos;
+using BookManagementWPFApp.Helpers;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace BookManagementWPFApp
 {
     public partial class PaymentWindow : Window
     {
-        // This class is the response data we get from PAYPAL generate token response
-        public class AuthorizationResponseData
+        private class OrderDetailVM
         {
-            public string scope { get; set; }
-            public string access_token { get; set; }
-            public string token_type { get; set; }
-            public string app_id { get; set; }
-            public int expires_in { get; set; }
-            public string[] supported_authn_schemes { get; set; }
-            public string nonce { get; set; }
-            public Client_Metadata client_metadata { get; set; }
+            public int OrderId { get; set; }
+            public string BookName { get; set; }
+            public string Author { get; set; }
+            public int Quantity { get; set; }
+            public string Desc { get; set; }
+            public double UnitPrice { get; set; }
+            public double TotalPrice { get; set; }
         }
-        public class Client_Metadata
-        {
-            public string name { get; set; }
-            public string display_name { get; set; }
-            public string logo_uri { get; set; }
-            public string[] scopes { get; set; }
-            public string ui_type { get; set; }
-        }
+        
+        private readonly PaymentHelper _paymentHelper;
+        private IOrderRepository _orderRepository = new OrderRepository();
+        private Order? _order;
 
-
-        private const string SandboxEmail = "sb-2kqvs34016732@business.example.com";
-        private const string pass = "VG/_qw2U";
-        private readonly HttpClient _httpClient;
-        IConfiguration configuration;
-        private string _clientId;
-        private string _clientSecret;
-        private string BaseURL = "https://api.sandbox.paypal.com/v1/oauth2/token";
         public PaymentWindow()
         {
             InitializeComponent();
+            _paymentHelper = new PaymentHelper();
             LoadData();
-            _httpClient = new HttpClient();
-            configuration = new ConfigurationBuilder()
-                .AddJsonFile("Credentials.json", true, true)
-                .Build();
-            _clientId = configuration["PAYPAL:CLIENTID"];
-            _clientSecret = configuration["PAYPAL:SECRET"];
+        }
+        // You should pass an in to this page so that it can load the order data
+        public PaymentWindow(Order order)
+        {
+            InitializeComponent();
+            _order = order;
+            _paymentHelper = new PaymentHelper();
+            LoadData();
         }
 
-        private void LoadData()
+        private async void LoadData()
         {
-
+            // Get the order
+            if (_order == null)
+            {
+                _order = await _orderRepository.GetOrderAsync(o => o.OrderID == 1);
+            }
+            // Load it to the data grid
+            IEnumerable<OrderDetailVM> orderDetails = from o in _order.OrderItems
+                select new OrderDetailVM
+                {
+                    OrderId = o.OrderID,
+                    Author = o.Book.Author.authorName,
+                    BookName = o.Book.Title,
+                    Desc = o.Book.Description,
+                    TotalPrice = o.Price,
+                    UnitPrice = o.Book.Price,
+                    Quantity = o.Quantity,
+                };
+            dgr_temp.ItemsSource = new ObservableCollection<OrderDetailVM>(orderDetails);
         }
 
         private async void btn_payClick_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                // Generate the access token first
-                var response = await GetAuthorizationResponse();
-                MessageBox.Show(response.access_token);
+                // Create request based on the order
+                var requestCreateDto = _paymentHelper.CreateOrderRequestDto(_order);
+                // Get the response
+                var response = await _paymentHelper.SendCreateOrderRequest(requestCreateDto);
+                // Navigate user to the browser
+                var approveLink = response.links.FirstOrDefault(l => l.rel.ToLower() == MyConstants.APPROVE.ToLower())
+                    .href;
+                var captureLink = response.links.FirstOrDefault(l => l.rel.ToLower() == MyConstants.CAPTURE.ToLower())
+                    .href;
+                System.Diagnostics.Process.Start(new ProcessStartInfo(approveLink) { UseShellExecute = true });
+                // update the order status after all things
+                _order.Status = MyConstants.STATUS_PENDING;
+                _orderRepository.UpdateOrder(_order);
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.Message, "An error has occured", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                LoadData();
             }
         }
 
-        private async Task<AuthorizationResponseData> GetAuthorizationResponse()
+        private async void btn_confirmBtn_Click(object sender, RoutedEventArgs e)
         {
-            var byteArray = Encoding.ASCII.GetBytes($"{_clientId}:{_clientSecret}");
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-
-            var keyValuePairs = new List<KeyValuePair<string, string>>
+            // Confirm the order of id 1 (Test only)
+            try
             {
-                new KeyValuePair<string, string>("grant_type", "client_credentials")
-            };
-
-            var response = await _httpClient.PostAsync(BaseURL, new FormUrlEncodedContent(keyValuePairs));
-            var responseAsString = await response.Content.ReadAsStringAsync();
-            var authorizationResp = JsonConvert.DeserializeObject<AuthorizationResponseData>(responseAsString);
-            return authorizationResp;
+                var result =
+                    await _paymentHelper.ConfirmOrder(_order.OrderID,
+                        _orderRepository); // confirm the order with id = 1
+                if (result)
+                {
+                    MessageBox.Show("Order confirmed, thank you for your purchase", "Confirmation", MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "An error has occured", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                LoadData();
+            }
         }
     }
 }
